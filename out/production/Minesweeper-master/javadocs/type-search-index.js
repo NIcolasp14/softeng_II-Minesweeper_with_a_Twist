2b5eaef47e2a8e71e83f84d5576ff2a20f94a494
nicolas <81325398+NIcolasp14@users.noreply.github.com>
2023-06-30 14:06:08 +0300

    Add files via upload

---
+++ out/production/Minesweeper-master/javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"InvalidDescriptionException"},{"p":"<Unnamed>","l":"InvalidValueException"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"scenarioReader"},{"p":"<Unnamed>","l":"scenarioWriter"},{"p":"<Unnamed>","l":"Tile"}];updateSearchResults();
